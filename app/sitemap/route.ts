936617b48bc1449d9fa7fd9d717a66d854527a40
triffyarts <businesspsalmsalcedo@gmail.com>
2025-12-31 12:37:27 +0800

    removed sitemap.ts changed to route.ts

--- app/sitemap/route.ts
+++ app/sitemap/route.ts
@@ -4,32 +4,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://triffyarts.vercel.app',
-      lastModified: new Date('2025-12-30T13:25:34.669Z'),
-      changeFrequency: 'weekly',
+      lastModified: new Date('2025-12-30'),
+      changeFrequency: 'weekly' as const,
       priority: 1,
     },
     {
       url: 'https://triffyarts.vercel.app/about',
-      lastModified: new Date('2025-12-30T13:25:34.669Z'),
-      changeFrequency: 'monthly',
+      lastModified: new Date('2025-12-30'),
+      changeFrequency: 'monthly' as const,
       priority: 0.8,
     },
     {
       url: 'https://triffyarts.vercel.app/projects',
-      lastModified: new Date('2025-12-30T13:25:34.669Z'),
-      changeFrequency: 'weekly',
+      lastModified: new Date('2025-12-30'),
+      changeFrequency: 'weekly' as const,
       priority: 0.9,
     },
     {
       url: 'https://triffyarts.vercel.app/arts',
-      lastModified: new Date('2025-12-30T13:25:34.669Z'),
-      changeFrequency: 'weekly',
+      lastModified: new Date('2025-12-30'),
+      changeFrequency: 'weekly' as const,
       priority: 0.9,
     },
     {
       url: 'https://triffyarts.vercel.app/contact',
-      lastModified: new Date('2025-12-30T13:25:34.669Z'),
-      changeFrequency: 'yearly',
+      lastModified: new Date('2025-12-30'),
+      changeFrequency: 'yearly' as const,
       priority: 0.7,
     },
   ]
